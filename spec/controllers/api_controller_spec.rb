46dd5c33c5d412393b782d90aa53c2998643a9bb
crichard1 <crichard1@uc.cl>
2016-05-13 16:21:11 -0300

    trying coverage rspec gem

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ApisController, type: :controller do
 	
-	 it "responds successfully with an HTTP 200 status code" do
-   	 require 'json'
+	it "responds successfully with an HTTP 200 status code" do
+   	require 'json'
     # expect(response).to have_http_status(200)
-    response = post :buscarTag,   :tag => 'bird'
+    post :buscarTag,   :tag => 'bird'
     expect(response).to have_http_status(200)
-    #hash = JSON.parse(response)
-    #expect(["metadata"].nil?)
+    expect(response.content_type).to eq "application/json"
     end
+
 
 
 end
